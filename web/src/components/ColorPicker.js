31202689e5688f3a826837e81cbb9ddda755e7ed
Anton Kharuzhy <anton@pricegrid.com>
2018-02-01 11:27:20 +0300

    Migration to TS

--- web/src/components/ColorPicker.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function ColorPicker(props) {
-    return <input type="color" value={props.value} onChange={props.onChange} />
-}
-
-export default ColorPicker;
--- web/webpack.development.config.js
+++
@@ -1,7 +0,0 @@
-import Config from 'webpack-config';
-
-export default new Config().extend('webpack.base.config.js').merge({
-    output: {
-        filename: 'index.js'
-    }
-});
