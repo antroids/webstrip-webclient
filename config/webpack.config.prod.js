911ed0a09d7131ffae6c045eebe26e8a71a7b619
Anton Kharuzhy <anton@pricegrid.com>
2018-02-11 21:16:46 +0300

    Travis config updated

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -13,6 +13,7 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin")
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -36,7 +37,8 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].[contenthash:8].css';
+//const cssFilename = 'static/css/[name].[contenthash:8].css';
+const cssFilename = 'static/css/[name].css';
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
@@ -64,8 +66,10 @@ module.exports = {
         // Generated JS file names (with nested folders).
         // There will be one main bundle, and one file per asynchronous chunk.
         // We don't currently advertise code splitting but Webpack supports it.
-        filename: 'static/js/[name].[chunkhash:8].js',
-        chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+        //filename: 'static/js/[name].[chunkhash:8].js',
+        //chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+        filename: 'static/js/[name].js',
+        chunkFilename: 'static/js/[name].chunk.js',
         // We inferred the "public path" (such as / or /my-project) from homepage.
         publicPath: publicPath,
         // Point sourcemap entries to original disk location (format as URL on Windows)
@@ -232,7 +236,8 @@ module.exports = {
                         // by webpacks internal loaders.
                         exclude: [/\.js$/, /\.html$/, /\.json$/],
                         options: {
-                            name: 'static/media/[name].[hash:8].[ext]',
+                            //name: 'static/media/[name].[hash:8].[ext]',
+                            name: 'static/media/[name].[ext]',
                         },
                     },
                     // ** STOP ** Are you adding a new loader?
@@ -342,6 +347,13 @@ module.exports = {
             async: false,
             tsconfig: paths.appTsConfig,
             tslint: paths.appTsLint,
+        }),
+        new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.js$|\.css$/,
+            threshold: 10240,
+            minRatio: 0.8
         }),
     ],
     // Some libraries import Node modules but don't use them in the browser.
